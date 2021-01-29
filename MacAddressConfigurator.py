d1d97bddee0675c4a1a82ad65b77d804f4f62c83
Anastasios Pantzartzis <37553665+apantzar@users.noreply.github.com>
2021-01-30 00:26:54 +0200

    Create MacAddressConfigurator.py
    V1

---
+++ MacAddressConfigurator.py
@@ -0,0 +1,52 @@
+
+Μετάβαση απευθείας στο περιεχόμενο
+Χρήση του Gmail με αναγνώστες οθόνης
+Meet
+Νέα σύσκεψη
+Συμμετοχή
+Hangouts
+Συζητήσεις
+Χρησιμοποιούνται 8,38 GB από το όριο των 100 GB
+Όροι · Απόρρητο · Πολιτικές προγράμματος
+Πρόσφατη δραστηριότητα λογαριασμού: πριν από 0 λεπτά
+Ανοικτός σε 1 άλλη τοποθεσία. · Λεπτομέρειες
+
+#!/usr/bin/env python
+import subprocess
+
+class color:
+  BLUE = '\033[96m'
+  DEFAULT = '\033[0m'
+
+difMac = "02:a0:04:d3:00:11"
+okStr = "---------------------------------------[OK]---------------------------------------"
+
+
+print("Installing/Updating net-tools...")
+subprocess.call("sudo apt install net-tools",shell=True);
+print(okStr)
+
+subprocess.call("sudo ifconfig",shell=True);
+
+interface=raw_input("Give the interface you want to configure: ")
+
+subprocess.call("sudo ifconfig "+interface+" down",shell=True);
+newMac=raw_input(color.BLUE +"Give new MAC or press (d) for default: "+color.DEFAULT)
+
+if(newMac == 'd' or newMac =='D'):
+    difMac = newMac
+    subprocess.call("sudo ifconfig "+interface+" hw ether " +difMac,shell=True);
+else:
+    difMac = "02:a0:04:d3:00:11"
+    subprocess.call("sudo ifconfig "+interface+" hw ether "+difMac,shell=True)
+
+
+subprocess.call("sudo ifconfig "+interface+" up",shell=True);
+
+print(okStr)
+
+
+print("Check new MAC: ")
+subprocess.call("sudo ifconfig",shell=True);
+
+
