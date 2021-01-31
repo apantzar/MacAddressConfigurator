8433fce43b30bd7380c8a33d4007e9471f08e8c4
apantzar <apantzar@csd.auth.gr>
2021-01-31 14:13:12 +0200

    Works in Kali : v2.0.1

--- MacAddressConfigurator.py
+++ MacAddressConfigurator.py
@@ -1,40 +1,36 @@
-
-#!/usr/bin/env python
+# !/usr/bin/env python
 import subprocess
 
+
 class color:
-  BLUE = '\033[96m'
-  DEFAULT = '\033[0m'
+    BLUE = '\033[96m'
+    DEFAULT = '\033[0m'
+
 
 difMac = "12:22:33:44:55:66"
 okStr = "---------------------------------------[OK]---------------------------------------"
 
-
 print("Installing/Updating net-tools...")
-subprocess.call("sudo apt install net-tools",shell=True);
+subprocess.call(" apt install net-tools", shell=True);
 print(okStr)
 
-subprocess.call("sudo ifconfig",shell=True);
+subprocess.call("ifconfig", shell=True)
 
-interface=raw_input("Give the interface you want to configure: ")
+interface = input("Give the interface you want to configure: ")
 
-subprocess.call("sudo ifconfig "+interface+" down",shell=True);
-newMac=raw_input(color.BLUE +"Give new MAC or press (d) for default: "+color.DEFAULT)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+newMac = input(color.BLUE + "Give new MAC or press (d) for default: " + color.DEFAULT)
 
-if(newMac == 'd' or newMac =='D'):
+if newMac == 'd' or newMac == 'D':
     difMac = newMac
-    subprocess.call("sudo ifconfig "+interface+" hw ether " +difMac,shell=True);
+    subprocess.call(" ifconfig " + interface + " hw ether " + difMac, shell=True)
 else:
     difMac = "12:22:33:44:55:66"
-    subprocess.call("sudo ifconfig "+interface+" hw ether "+difMac,shell=True)
+    subprocess.call(" ifconfig " + interface + " hw ether " + difMac, shell=True)
 
-
-subprocess.call("sudo ifconfig "+interface+" up",shell=True);
+subprocess.call("sudo ifconfig " + interface + " up", shell=True)
 
 print(okStr)
 
-
 print("Check new MAC: ")
-subprocess.call("sudo ifconfig",shell=True);
-
-
+subprocess.call("sudo ifconfig", shell=True)
